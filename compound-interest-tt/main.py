4f2fe165311582e0db25ebbd08661c17dba372ec
guepardeau <hassanakhalid22@gmail.com>
2025-12-17 10:13:49 +0000

    validate principal input

--- compound-interest-tt/main.py
+++ compound-interest-tt/main.py
@@ -3,10 +3,19 @@ while True:
     if currency == "$" or currency == "£" or currency == "€":
         break
     else:
-        print("Please input either $, £ or €: ")
+        print("Please input either $, £ or €:")
 
 
-principal = None
+while True:
+    try:
+        principal = (float(input("Please enter Principal (e.g. 10500)")))
+        if principal > 0:
+            break
+        else:
+            print("Principal must be greater than 0: ")
+    except ValueError:
+        print("Please enter a Numerical Value only: ")
+
 interest_rate = None
 payouts_per_year = None
 time = None
